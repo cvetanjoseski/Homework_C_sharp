d103e650ac1d64fbc5bf2c2c51e5873ddbdf6618
cvetanjoseski <cvetanjoseski@gmail.com>
2022-03-30 22:00:08 +0200

    Homework

---
+++ Homework_2/Homework_2/Task1/SumOfEven.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2.Task1
+{
+    class SumOfEven
+    {
+        static void Main(string[] args)
+        {
+            string inputNum;
+            int[] arrayOfNumbers = new int[6];
+            int sumOfEvenNumbers = 0;
+            Console.WriteLine("Enter array of 6 numbers, one by one");
+            for (int counter = 0; counter < 6; counter++)
+            {
+                inputNum = Console.ReadLine();
+                bool isInputOk = int.TryParse(inputNum, out int parsedNumber);
+                if (isInputOk)
+                {
+                    arrayOfNumbers[counter] = parsedNumber;
+                    if (arrayOfNumbers[counter] % 2 == 0)
+                    {
+                        sumOfEvenNumbers += arrayOfNumbers[counter];
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You didnt enter number!");
+                }
+            }
+            Console.WriteLine($"Sum of all even numbers in array is {sumOfEvenNumbers}");
+         }
+    }
+
+}
---
+++ Homework_2/Task2/Task2/StudentGroup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2.Task2
+{
+    class StudentGroup
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsGOne = { "Stefan", "Petar", "Simona", "Renata", "Hristijan" };
+            string[] studentsGTwo = { "Cvetan", "David", "Petar", "Hristina", "Stefani" };
+            string inputGroupCheck;
+            Console.WriteLine("Which group students you want to print? 1/2");
+            inputGroupCheck = Console.ReadLine();
+            if(inputGroupCheck == "1")
+            {
+                Console.WriteLine("Students in group One are: ");
+                for(int counter = 0; counter<5; counter++)
+                {
+                    Console.WriteLine($"{studentsGOne[counter]}");
+                }
+            }
+            else if(inputGroupCheck == "2")
+            {
+                Console.WriteLine("Students in group Two are: ");
+                for (int counter = 0; counter < 5; counter++)
+                {
+                    Console.WriteLine($"{studentsGTwo[counter]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You enter wrong group number");
+            }
+        }
+    }
+}
